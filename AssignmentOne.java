2dfe7851a2f0f09bbaa5e6efe783dd9ec873afe5
Abby Ramos Cortez <123671798+aramoscortez@users.noreply.github.com>
2023-06-14 01:53:42 -0400

    Printing out stuff in the Command Prompt

---
+++ AssignmentOne.java
@@ -0,0 +1,32 @@
+/**
+* Assignment One - Getting to Know You 
+*
+* @author Abby Ramos Cortez
+*
+* @version 19.0.1 2023-01-19
+*
+ */
+
+public class AssignmentOne
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\n1. What is your full name?");
+        System.out.println("Abicenia Adely Ramos Cortez\n");
+        System.out.println("2. What is your preferred name?");
+        System.out.println("Abby Ramos Cortez\n");
+        System.out.println("3. What is your favorite food?");
+        System.out.println("Beyond Meat Burgers\n");
+        System.out.println("4. Do you have any pets? If so, tell me a little about them.");
+        System.out.println("No, I don't. Although I wish I had a Golden Retriever.\n");
+        System.out.println("5. What is something fun that you did over break?");
+        System.out.println("I watched a marathon of How to get Away with Murder.\n");
+        System.out.println("6. What types of music do you like?");
+        System.out.print("I listen to a variety of music ranging from R&B, Hip Hop, Pop, Bachata, ");
+        System.out.println("Reggaeton, Cumbia and much more.\n");
+        System.out.println("7. Who do you admire or look up to?");
+        System.out.println("I admire/look up to my parents.\n");
+        System.out.println("8. If you could have a single superpower, what would it be and why?");
+        System.out.println("My superpower would be magic because I could perform all sorts of spells.\n");
+    }
+}
